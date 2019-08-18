c22ca909863f072824a5404a5a216833fab999c3
coderbean <371327964@qq.com>
2019-08-18 21:32:08 +0800

    阿里面试题

---
+++ src/thread/Main2.java
@@ -0,0 +1,35 @@
+package thread;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by jacob on 2019-08-18.
+ */
+public class Main2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[3]; // 0,1,2 分别是 A、B、C 线程
+        Semaphore[] semaphores = new Semaphore[3];
+        for (int i = 0; i < semaphores.length; i++) {
+            semaphores[i] = new Semaphore(1);
+            semaphores[i].acquire();
+        }
+        semaphores[0].release();
+        for (int i = 0; i < threads.length; i++) {
+            Semaphore semaphore = semaphores[i];
+            Semaphore nextSemaphore = i == semaphores.length - 1 ? semaphores[0] : semaphores[i + 1];
+            threads[i] = new Thread(() -> {
+                try {
+                    for (;;) {
+                        semaphore.acquire();
+                        System.out.println(Thread.currentThread().getName());
+                        nextSemaphore.release();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            threads[i].setName(((char) ('A' + i)) + "");
+            threads[i].start();
+        }
+    }
+}
---
+++ src/thread/README.md
@@ -0,0 +1,10 @@
+##问题
+编写⼀一个程序，开启3个线程呢个，这3个线程的ID分别为ABC每个线程将⾃⼰的ID在屏幕上打印10遍，要求 输出结果必须按ABC的顺序显示，如:ABCABC。。依次递推
+
+## 思路
+1. 使用 Condition
+2. 使用 Semaphore
+
+## 参考
+[咖啡拿铁](https://mp.weixin.qq.com/s/Qdzbyow8AWCDLn6RABwo9w)
+
